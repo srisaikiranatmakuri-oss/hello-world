c8a0457d4247c3c8b2ddfb4a6dd1b822e2cd7753
srisaikiranatmakuri-oss <srisaikiranatmakuri@gmail.com>
2025-10-05 00:10:11 -0500

    Update huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -17,19 +17,19 @@ struct Node {
     }
 };
 
-// Comparison object for priority queue (min-heap)
+
 struct Compare {
     bool operator()(Node* l, Node* r) {
         return l->freq > r->freq;
     }
 };
 
-// Function to print Huffman Codes using preorder traversal
+
 void printCodes(Node* root, string code) {
     if (!root)
         return;
 
-    // Print the code if it's a leaf node
+  
     if (!root->left && !root->right) {
         cout << code << " ";
         return;
@@ -39,16 +39,16 @@ void printCodes(Node* root, string code) {
     printCodes(root->right, code + "1");
 }
 
-// Function to build Huffman Tree and print codes
+
 void HuffmanCodes(char S[], int f[], int N) {
     priority_queue<Node*, vector<Node*>, Compare> pq;
 
-    // Step 1: Create leaf nodes for each character and push into priority queue
+  
     for (int i = 0; i < N; i++) {
         pq.push(new Node(S[i], f[i]));
     }
 
-    // Step 2: Build the tree
+   
     while (pq.size() > 1) {
         Node* left = pq.top(); pq.pop();
         Node* right = pq.top(); pq.pop();
@@ -60,11 +60,11 @@ void HuffmanCodes(char S[], int f[], int N) {
         pq.push(newNode);
     }
 
-    // Step 3: Print Huffman Codes in preorder
+   
     printCodes(pq.top(), "");
 }
 
-// Main function
+
 int main() {
     string S = "abcdef";
     int f[] = {5, 9, 12, 13, 16, 45};
